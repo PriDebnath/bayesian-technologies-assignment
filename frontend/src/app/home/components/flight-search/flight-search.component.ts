ee1910fd00ae3e7c3300fbf4681e7ab064b9b3e9
Pritam <debnathpritam0802@gmail.com>
2024-08-09 19:45:15 +0530

    set routing for home

--- frontend/src/app/home/components/flight-search/flight-search.component.ts
+++ frontend/src/app/home/components/flight-search/flight-search.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
-  styleUrl: './flight-search.component.css'
+  styleUrl: './flight-search.component.css',
 })
 export class FlightSearchComponent {
+  originList: string[] = ['JFK', 'DEL', 'SYD', 'BOM', 'BNE', 'BLR'];
 
+  flightForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.flightForm = this.fb.group({
+      origin: ['', Validators.required],
+      destination: ['', Validators.required],
+      cabin: ['economy', Validators.required],
+      proFilters: [false],
+    });
+  }
+
+  onSearch() {
+    if (this.flightForm.valid) {
+      // Implement your search logic here
+      console.log(this.flightForm.value);
+    }
+  }
 }
--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlightSearchComponent } from './components/flight-search/flight-search.component';
-
-
-
+import { HomeRoutingModule } from './home-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @NgModule({
-  declarations: [
-    FlightSearchComponent
-  ],
+  declarations: [FlightSearchComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HomeRoutingModule, // its routing
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
